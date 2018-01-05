8d1f88eba2fe96da1c623bbe9ae021df8fec390f
BenWalsh <bwalsh@avoka.com>
2018-01-05 20:02:34 +1100

    Initial commit

---
+++ libraries/templates-library/1.0.0/widgets/docking-progress-bar/docking-progress-bar.js
@@ -0,0 +1,26 @@
+define(["app"], function(app) {    
+        app.ng.controller("DockingProgressBarController", ["$scope", "$element", "Form", "Resource", "Validation", "Util", "$timeout",
+            function($scope, $element, Form, Resource, Validation, Util, $timeout) {    
+              Form.getItem($scope, $element).then(function (item) {
+				var data = $scope.data;
+				$scope.$logo = $('.id-logo .av-image');
+				$scope.$header = $('.id-formHeader');
+				$scope.$banner = $element.find('.av-progress-bar-banner');
+				$scope.$navContainer = $element.find('.av-docking-block');
+				$scope.$watch("data.scrollPosition", function(newValue, oldValue) {
+				  var lw = $scope.$logo.width();
+				  var hh = $scope.$header.outerHeight();
+				  var sp = newValue;
+				  var dp = Form.getItemProperty("data.dockingController","dockPosition");
+				  var ds = hh * (1 - sp/dp);
+				  var relativeMargin = $(window).width() * 0.001;
+				  $scope.$navContainer.css({"width": "calc(100% + " + (lw*(1 - Math.pow(sp/dp, 0.2)) - ((1 - ds/hh) * relativeMargin)) + "px)"});
+				  //$scope.$navContainer.css({"width": "calc(100% + " + (lw*(1 - Math.pow(sp/dp, 0.2))) + "px)"});
+				  $scope.$navContainer.css({"top": ds + "px"});
+				  $scope.$banner .css({"height": ds + "px"});
+				 });
+				data.scrollPosition = 0;
+			  });
+            }
+        ]);
+    });
