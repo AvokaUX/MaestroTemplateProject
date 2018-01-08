245383699655c7389b84600be7c63e1ced8a8806
Jun Sun <jsun@avoka.com>
2018-01-08 15:29:34 +1100

    cc

--- libraries/templates/1.0.0/scripts/script.js
+++ libraries/templates/1.0.0/scripts/script.js
@@ -1,5 +1,6 @@
 var $scope = $("form").scope();
 console.log('javascript is working');
+console.log('javascript is not working');
 console.log('$scope', $scope);
 $scope.$watch('Form.lastFocusedItem', function() {
   console.log('hey, Form.lastFocusedItem has changed!');
