46f708e884ab0159a8866b1e7c42793e8119e1a7
BenWalsh <bwalsh@avoka.com>
2018-03-09 12:02:07 +1100

    Docking fixes

--- libraries/core-templates/1.0.0/widgets/docking-errors/docking-errors.js
+++ libraries/core-templates/1.0.0/widgets/docking-errors/docking-errors.js
@@ -4,12 +4,13 @@ define(["app"], function(app) {
               Form.getItem($scope, $element).then(function (item) {
 				var data = $scope.data;
 				$scope.dp = Form.getItemProperty("data.dockingController","dockPosition");
+				$scope.pbh = Form.getItemProperty("data.dockingProgressBar","progressBarHeight");
 				$scope.$watch("data.scrollPosition", function(newValue, oldValue) {
 					if (!data.errorsDockLock) {
                         data.dockErrors = (!($(".av-menu-errors:visible").length > 0) || !data.undocked);
                     }
 					//$element.css({"top": item.properties.progressBarHeight * (1 - newValue/$scope.dp) + "px"});
-                    $element.css({"top": 50 * (1 - newValue/$scope.dp) + "px"});
+                    $element.css({"top": $scope.pbh * (1 - newValue/$scope.dp) + "px"});
 				 });
 			  });
             }
