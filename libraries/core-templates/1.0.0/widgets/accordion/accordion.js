0d25432dd3737646ae84c60c747ea42ac54fe29a
jsun <sjmonkey9@hotmail.com>
2018-03-14 11:14:39 +1100

    add open next to accordion

--- libraries/core-templates/1.0.0/widgets/accordion/accordion.js
+++ libraries/core-templates/1.0.0/widgets/accordion/accordion.js
@@ -1,8 +1,16 @@
 define(["app"], function (app) {
 	app.ng.controller("avAccordionController", ["$scope", "Form", "$rootScope", "Util", "Validation", "Resource", "$element", "$timeout", "$filter", function ($scope, Form, $rootScope, Util, Validation, Resource, $element, $timeout, $filter) {
 		Form.getItem($scope, $element).then(function (item) {
-			$scope.$on("accordionOpening", function(evt, data) {                
+            var children = Util.children(item);
+            $scope.$on("accordionOpening", function(evt, data) {
                 $scope.$broadcast("siblingOpening", data);
+            });
+            $scope.$on("openNext", function(evt, data) {
+            	for (var i = 0; i < children.length; i++) {
+            		if (data === children[i].id) {
+                        $scope.$broadcast("openThisOne", children[i+1].id);
+					}
+				}
             });
 		});
 	}]);
