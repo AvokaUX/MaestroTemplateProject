8f90288f168e9520aff6d1b94288415fdfc36e36
csilva <csilva@avoka.com>
2018-03-16 16:08:00 +1100

    empty

--- libraries/core-templates/1.0.0/widgets/nav-buttons/nav-buttons.js
+++
@@ -1,8 +0,0 @@
-define(["app"], function (app) {
-    app.ng.controller("avNavButtonsController", ["$scope", "Form", "$element", "Util", "$timeout", "$filter", function ($scope, Form, $element, Util, $timeout, $filter) {
-        Form.getItem($scope, $element).then(function (item) {
-            var data = $scope.data;
-            data.blockNext = "";
-        });
-    }]);
-});
